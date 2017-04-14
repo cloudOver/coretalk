fce5218be4822ad37bfe5054f93ed31d5cc11108
Maciej Nabozny <maciej.nabozny@cloudover.io>
2017-04-14 22:32:27 +0200

    Updated header

--- coretalk/settings.py
+++ coretalk/settings.py
@@ -1,22 +1,30 @@
 """
-Copyright (c) 2014 Maciej Nabozny
+Copyright (C) 2014-2017 cloudover.io ltd.
+This file is part of the CloudOver.org project
 
-This file is part of CloudOver project.
+Licensee holding a valid commercial license for this software may
+use it in accordance with the terms of the license agreement
+between cloudover.io ltd. and the licensee.
+ 
+Alternatively you may use this software under following terms of
+GNU Affero GPL v3 license:
 
-CloudOver is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version. For details contact
+with the cloudover.io company: https://cloudover.io/
+
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+
+You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
 
 from corecluster.settings import *
 
